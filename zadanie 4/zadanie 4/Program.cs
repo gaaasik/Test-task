1911c2d99bac4a417b5fa58407d822236c079c5b
gaaasik <32246971+gaaasik@users.noreply.github.com>
2019-06-30 17:36:39 +0300

    Add files via upload

---
+++ zadanie 4/zadanie 4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_4
+{
+    class Program
+    {   static void Factorial(int a)
+        {
+            Console.Write(a+"!=");
+
+            double f = 1;
+            while (a > 1) // нахождение факториала
+            {
+                f = a * f;
+                a -= 1;
+            }
+            
+
+            Console.Write(f);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число = ");
+          
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Factorial(a);
+            Console.ReadLine();
+
+        }
+
+    }
+}
