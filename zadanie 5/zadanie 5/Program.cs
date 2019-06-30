0dece65e580e2d57a6fd34c3ce30190ab39489a2
gaaasik <32246971+gaaasik@users.noreply.github.com>
2019-06-30 14:48:31 +0300

    Add files via upload

---
+++ zadanie 5/zadanie 5/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace zadanie_5
+{
+    class Program
+    {
+        
+public class KeyEventArgs : EventArgs
+        {
+
+        }
+
+        static void CheckStr(string[] str)
+
+
+        {
+
+            char[] symbol = new char[100];
+            int k = 0;
+            int brackets = 0;
+            int brackets1 = 0;
+            int brackets2 = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                foreach (char c in str[i])
+                {
+                    symbol[i] = c;
+
+
+
+                    if (symbol[i] == '(')
+                        brackets += 1;
+
+                    if (symbol[i] == '{')
+                        brackets1 += 1;
+
+                    if (symbol[i] == '[')
+                        brackets2 += 1;
+
+                    if (symbol[i] == ')' )
+                        brackets -= 1;
+
+                    if (symbol[i] == '}')
+                        brackets1 -=1;
+
+                    if (symbol[i] == ']')
+                        brackets2 -= 1;
+
+                    
+                }
+
+
+            
+            }
+            if (brackets == 0 && brackets1 == 0 && brackets2 == 0)
+            {
+                Console.WriteLine("Строка является правильной скобочной последовательностью!");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("НЕПРАВИЛЬНО");
+                Console.WriteLine();
+            }
+
+        }    
+        static void Main(string[] args)
+        {
+            int t = 3;
+            
+            again:
+
+            Console.WriteLine("Введите текст");
+            string[] str = Console.ReadLine().Split(',',' ');
+            CheckStr(str);
+
+            Console.WriteLine("Если хотите еще нажмите 1");
+
+
+
+            if (t != 1)
+            {
+                t -= 1;
+                goto again;
+            }
+        }
+    }
+}
